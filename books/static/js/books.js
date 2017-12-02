679a8ddefd155bd39608808105e2f2bae437e289
Ilham Akbar <ilhamije@gmail.com>
2017-12-03 02:00:10 +0700

    Added - Update Book Item

--- books/static/js/books.js
+++ books/static/js/books.js
@@ -1,8 +1,9 @@
 $(function () {
+  // CREATE ITEM
   $(".js-create-book").click(function () {
-    // console.log('kampret');
+    var btn = $(this);
     $.ajax({
-      url: '/books/create/',
+      url: btn.attr("data-url"),
       type: 'get',
       dataType: 'json',
       beforeSend: function () {
@@ -14,7 +15,23 @@ $(function () {
     });
   });
 
-  $("#modal-book").on("submit", ".js-book-create-form", function () {
+  // EDIT ITEM
+  var loadForm = function () {
+    var btn = $(this);
+    $.ajax({
+      url: btn.attr("data-url"),
+      type: 'get',
+      dataType: 'json',
+      beforeSend: function () {
+        $("#modal-book").modal("show");
+      },
+      success: function (data) {
+        $("#modal-book .modal-content").html(data.html_form);
+      }
+    });
+  };
+
+  var saveForm = function () {
     var form = $(this);
     $.ajax({
       url: form.attr("action"),
@@ -23,7 +40,6 @@ $(function () {
       dataType: 'json',
       success: function (data) {
         if (data.form_is_valid) {
-          // alert('Book created!');
           $("#book-table tbody").html(data.html_book_list);
           $("#modal-book").modal("hide");
         }
@@ -33,6 +49,16 @@ $(function () {
       }
     });
     return false;
-  });
+  };
+
+  /* Binding */
+
+  // Create book
+  $(".js-create-book").click(loadForm);
+  $("#modal-book").on("submit", ".js-book-create-form", saveForm);
+
+  // Update book
+  $("#book-table").on("click", ".js-update-book", loadForm);
+  $("#modal-book").on("submit", ".js-book-update-form", saveForm);
 
 });
--- books/views.py
+++ books/views.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+
 from .models import Book
 from .forms import BookForm
+
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book/book_list.html', {'books':books})
 
-def book_create(request):
+def save_book_from(request, form, template_name):
     data = dict()
-
     if request.method == 'POST':
-        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             data['form_is_valid'] = True
@@ -25,12 +25,22 @@ def book_create(request):
             )
         else:
             data['form_is_valid'] = False
-    else:
-        form = BookForm()
 
     context = {'form': form}
-    data['html_form'] = render_to_string('book/partial_book_create.html',
-        context,
-        request=request,
-    )
+    data['html_form'] = render_to_string(template_name, context, request=request)
     return JsonResponse(data)
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+    else:
+        form = BookForm()
+    return save_book_from(request, form, 'book/partial_book_create.html')
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+    else:
+        form = BookForm(instance=book)
+    return save_book_from(request, form, 'book/partial_book_update.html')
