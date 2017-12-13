234ae4cf0cbce62fb3f27ae9919aeb0fef844978
Ilham Akbar <ilhamije@gmail.com>
2017-12-14 00:21:11 +0700

    Update database & requirements

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,10 +85,9 @@ DATABASES = {
     }
 }
 
-# DATABASES = dj_database_url.parse('postgres://bookstore_user:bookstore_pass@127.0.0.1:5432/bookstore_db')
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
